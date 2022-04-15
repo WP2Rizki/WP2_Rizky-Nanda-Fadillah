113a18914fb2edfeea54fa667884ce3bb900f838
WP2Rizki <102018273+WP2Rizki@users.noreply.github.com>
2022-04-15 20:11:00 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,27 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Rizki Nanda </li>
+        <li>Nama Belakang : Fadilah</li>
+        <li>NIM : 17190294</li>
+    </ul>
+
+    <h4> Alamat </h4>
+    <ul type="none">
+        <li>kemiri jaya RT01/01</li> 
+        <li>Kel.beji, Kec.beji , Kab.JawaBarat</li>
+    </ul>
+
+    <h4> Tempat Lahir </h4>
+    <ul type="none">
+        <li>Depok</li>
+    </ul>
+
+    <h4>Hobi</h4>
+    <ul type="square">
+        <li>Bermain Futsal</li>
+        <li>Bermain Game</li>
+        <li>Mendengarkan puisi</li>
+    </ul>
+</section>
